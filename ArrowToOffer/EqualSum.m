f06508d46068b9559c672d931a18779368860cb2
xiaochen yang <xiaochenyung@gmail.com>
2017-11-15 17:29:36 +0800

    add comment

--- ArrowToOffer/EqualSum.m
+++ ArrowToOffer/EqualSum.m
@@ -16,28 +16,31 @@
     }
     NSInteger left = 0;
     NSInteger right = array.count - 1;
-    NSNumber *tempRightNumber = array[right];
+    NSNumber *tempRightNumber = array[right];//取出数组最右侧的值
     NSInteger tempRightValue = tempRightNumber.integerValue;
-    while (tempRightValue > value) {
+    while (tempRightValue > value) {//待比较值与最右侧值做比较,一直找到比待比较值小的数，这样会比较节约时间
         --right;
         tempRightNumber = array[right];
         tempRightValue = tempRightNumber.integerValue;
     }
     NSNumber *tempLeftNumber = array[left];
     NSInteger tempLeftValue = tempLeftNumber.integerValue;
-    while (tempLeftValue + tempRightValue < value) {
+    while (tempLeftValue + tempRightValue < value) {//去掉最小的不可能用到的数据
         ++left;
         tempLeftNumber = array[left];
         tempLeftValue = tempLeftNumber.integerValue;
     }
-    if (tempLeftValue + tempRightValue == value) {
-        NSLog(@"最小的值为%zu, 最大的值为%zu",tempLeftValue, tempRightValue);
-        return true;
-    } else {
-        if (right - left - 1 > left) {
-            [self equalSubWithArray:[array subarrayWithRange:NSMakeRange(left, right - left - 1)] value:value];
+    while (left < right) {
+        if (tempLeftValue + tempRightValue == value) {//如果想等自然最好，打印两个值
+            NSLog(@"最小的值为%zu, 最大的值为%zu",tempLeftValue, tempRightValue);
+            return true;
+        } else if (tempLeftValue + tempRightValue > value) {//玩大了那右边指针前移
+            --right;
+        } else {
+            ++left;//玩小了左边指针后移
         }
     }
+    
     return false;
 }
 
