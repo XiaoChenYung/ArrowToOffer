6a9caab3b087254bc1438d0db486b1fdf54e5679
xiaochenyung <xiaochenyung@gmail.com>
2018-01-31 14:12:00 +0800

    冒泡

--- ArrowToOffer/main.m
+++ ArrowToOffer/main.m
@@ -7,23 +7,34 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "BinaryTreeNode.h"
-#import "EqualSum.h"
+
+void bubbleSort(int array[], int len);
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        [EqualSum equalSubWithArray:@[@1, @3, @4, @8, @9] value:12];
-        // insert code here...
-        BinaryTreeNode *root = [BinaryTreeNode createBinaryTreeWothValues:@[@2, @1, @7, @0, @2, @5, @11, @(-1), @1, @2, @3, @5, @6, @10, @13]];
-//        BinaryTreeNode *node = [BinaryTreeNode findNodeAtIndex:12 inRootNode:root];
-//        [BinaryTreeNode levelTraverseTreeNode:root handler:^(BinaryTreeNode *rootNode) {
-//            NSLog(@"%zu", rootNode.value);
-//        }];
-//        NSInteger depth = [BinaryTreeNode widthOfTree:root];
-        BinaryTreeNode *node = root.leftNode.rightNode;
-        NSArray *path = [BinaryTreeNode pathForNode:node inTree:root];
-        NSLog(@"哈哈");
-        
+        int array[] = {8, 3, 6, 2, 1, 5, 4, 4, 9, 0};
+        bubbleSort(array, 10);
+        for (int i = 0; i < 10; i ++) {
+            printf("%d\n",array[i]);
+        }
     }
     return 0;
+}
+
+/**
+ 冒泡排序
+
+ @param array 输入的数组
+ @param len 数组大小
+ */
+void bubbleSort(int array[], int len) {
+    for (int i = 0; i < len - 1; i ++) {
+        for (int j = 0; j < len - i - 1; j ++) {
+            if (array[j] > array[j + 1]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
 }
